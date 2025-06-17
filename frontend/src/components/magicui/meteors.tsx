211b537bdcd34759ad8e58208a8b909552ea15b9
gucci1909 <umangarora0134@gmail.com>
2025-06-17 10:40:16 +0530

    error remove

--- frontend/src/components/magicui/meteors.tsx
+++ frontend/src/components/magicui/meteors.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "../../lib/utils";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 interface CoachingBubblesProps {
   number?: number;
